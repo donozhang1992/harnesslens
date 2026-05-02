53134c7fcce407ff1c65f74c22cee923e2915ca1
dapeng <dapengzhang199@gmail.com>
2026-05-02 14:46:10 +1000

    test data augmentation in CNN

--- pre-lab-conda/model_training/keras_cnn/main.py
+++ pre-lab-conda/model_training/keras_cnn/main.py
@@ -9,11 +9,25 @@ dropout_rate = 0.2
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 x_train = x_train.astype("float32") / 255.0 # normalization
 x_test = x_test.astype("float32") / 255.0 # normalization
+x_train_small = x_train[:1000]
+y_train_small = y_train[:1000]
 
 model = models.Sequential([
-    layers.Flatten(input_shape=(28,28)),
-    layers.Dense(128, activation='relu'),
-    layers.Dropout(dropout_rate),
+    layers.Reshape((28,28,1), input_shape=(28,28)),
+    
+    # Data augmentation
+    layers.RandomRotation(0.1),
+    layers.RandomZoom(0.1),
+    layers.RandomFlip("horizontal"),
+    
+    layers.Conv2D(32, (3,3), activation='relu'),
+    layers.MaxPooling2D((2,2)),
+    
+    layers.Conv2D(64, (3,3), activation='relu'),
+    layers.MaxPooling2D((2,2)),
+    
+    layers.Flatten(),
+    layers.Dense(64, activation='relu'),
     layers.Dense(10)
 ])
 
@@ -24,21 +38,26 @@ model.compile(
     metrics=['accuracy']
 )
 
+import time
+start = time.time()
 # training
 history = model.fit(
-    x_train,
-    y_train,
+    # x_train,
+    # y_train,
+    x_train_small,
+    y_train_small,
     epochs=5,
     batch_size=64,
     validation_split=0.1
 )
-
-plot_history(history, dropout_rate, f"dense_{dropout_rate}.png")
+end = time.time()
+plot_history(history, dropout_rate, f"cnn_{dropout_rate}_{time.strftime('%Y_%m_%d_%H_%M_%S', time.localtime(end))}.png")
 
 # testing
 test_loss, test_acc = model.evaluate(x_test, y_test)
 print(f"Test loss: {test_loss}")
 print(f"Test accuracy: {test_acc}")
+print(f"Training time: {end - start:.2f} seconds")
 
 
 model.save("mnist_model.keras")
--- pre-lab-conda/model_training/keras_dense/main.py
+++ pre-lab-conda/model_training/keras_dense/main.py
@@ -3,42 +3,47 @@ from tensorflow.keras import layers, models
 from tensorflow.keras.datasets import mnist
 from utils.plot import plot_history
 
-dropout_rate = 0.2
-
 # loading data
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 x_train = x_train.astype("float32") / 255.0 # normalization
 x_test = x_test.astype("float32") / 255.0 # normalization
+def main(dropout_rate):
+    model = models.Sequential([
+        layers.Flatten(input_shape=(28,28)),
+        layers.Dense(128, activation='relu'),
+        layers.Dropout(dropout_rate),
+        layers.Dense(10)
+    ])
 
-model = models.Sequential([
-    layers.Flatten(input_shape=(28,28)),
-    layers.Dense(128, activation='relu'),
-    layers.Dropout(dropout_rate),
-    layers.Dense(10)
-])
+    # compile
+    model.compile(
+        optimizer = 'adam',
+        loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+        metrics=['accuracy']
+    )
 
-# compile
-model.compile(
-    optimizer = 'adam',
-    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-    metrics=['accuracy']
-)
+    import time
+    start = time.time()
+    # training
+    history = model.fit(
+        x_train,
+        y_train,
+        epochs=5,
+        batch_size=64,
+        validation_split=0.1
+    )
+    end = time.time()
+    plot_history(history, dropout_rate, f"dense_{dropout_rate}.png")
 
-# training
-history = model.fit(
-    x_train,
-    y_train,
-    epochs=5,
-    batch_size=64,
-    validation_split=0.1
-)
-
-plot_history(history, dropout_rate, f"dense_{dropout_rate}.png")
-
-# testing
-test_loss, test_acc = model.evaluate(x_test, y_test)
-print(f"Test loss: {test_loss}")
-print(f"Test accuracy: {test_acc}")
+    # testing
+    test_loss, test_acc = model.evaluate(x_test, y_test)
+    print(f"Test loss: {test_loss}")
+    print(f"Test accuracy: {test_acc}")
+    print(f"Training time: {end - start:.2f} seconds")
 
 
-model.save("mnist_model.keras")
+# main(0.0)
+# main(0.2)
+# main(0.5)
+main(1.0)
+# model.save("mnist_model.keras")
